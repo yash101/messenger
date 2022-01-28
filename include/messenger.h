bec80b52490f54d3019bda9dcb95b7dd168ca74a
yash101 <lodhad@gmail.com>
2022-01-27 22:48:23 -0700

    making good progress

--- include/messenger.h
+++ include/messenger.h
@@ -3,23 +3,52 @@
 
 #include <stdint.h>
 
+typedef int socket_handle_t;
+
 typedef struct messenger_error_t {
   int status;
   const char* message;
 } messenger_error_t;
 
 typedef struct messenger_client_t {
+  struct sockaddr_in* addr_local;
+  struct sockaddr_in* addr_remote;
+
+  struct sockaddr_in6* addr_local6;
+  struct sockaddr_in6* addr_remote6;
+  
+  socket_handle_t socket;
 } messenger_client_t;
 
 typedef struct messenger_server_t {
+  struct sockaddr_in* addr_local;
+  struct sockaddr_in6* addr_local6;
+  
+  socket_handle_t socket;
+  socket_handle_t socket6;
+  uint16_t port;
 } messenger_server_t;
 
-messenger_error_t messenger_create_error(messenger_error_t* error, int status);
-const char* messenger_get_error_msg(int errno);
+/*
+  Error management
+*/
+messenger_error_t messenger_error_create(messenger_error_t* error, int status);
+const char* messenger_error_msg_get(int errno);
 
-void messenger_create_server(messenger_server_t* server);
-void messenger_create_client(messenger_client_t* server);
-void messenger_bind_server(messenger_server_t* server, uint16_t port);
-void messenger_connect_client(messenger_client_t* client, const char* address, uint16_t port);
+/*
+  Server
+*/
+messenger_error_t messenger_server_create(messenger_server_t* server);
+void messenger_server_destroy(messenger_server_t* server);
+
+void messenger_server_bind(messenger_server_t* server, uint16_t port);
+
+/*
+  Client
+*/
+messenger_error_t messenger_client_create(messenger_client_t* client);
+void messenger_client_destroy(messenger_client_t* client);
+
+void messenger_client_connect(messenger_client_t* client, const char* address, uint16_t port);
 
 #endif
--- src/main/messenger_capi.c
+++ src/main/messenger_capi.c
@@ -1,13 +1,21 @@
 #include "messenger.h"
 
 #include <string.h>
+#include <stdlib.h>
+
+#include <unistd.h>
+#include <arpa/inet.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
 
 const char* error_messages[] =
 {
-  "Success"
+  "Success",
+  "Bad allocation",
+  "Socket create failure"
 };
 
-const char* messenger_get_error_msg(int errno)
+const char* messenger_error_msg_get(int errno)
 {
   if (errno >= sizeof(error_messages) / sizeof(error_messages[0]))
   {
@@ -17,7 +25,7 @@ const char* messenger_get_error_msg(int errno)
   return error_messages[errno];
 }
 
-messenger_error_t messenger_create_error(
+messenger_error_t messenger_error_create(
   messenger_error_t* error,
   int status
 )
@@ -27,23 +35,120 @@ messenger_error_t messenger_create_error(
     error = &err;
 
   error->status = status;
-  error->message = messenger_get_error_msg(status);
+  error->message = messenger_error_msg_get(status);
 
   return *error;
 }
 
-void messenger_create_server(messenger_server_t* server)
+messenger_error_t messenger_server_create(messenger_server_t* server)
 {
+  server->socket = 0;
+  server->addr_local = malloc(sizeof(struct sockaddr_in));
+  server->addr_local6 = malloc(sizeof(struct sockaddr_in6));
+
+  if (
+    !server->addr_local ||
+    !server->addr_local6
+  )
+  {
+    return messenger_error_create(NULL, 1);
+  }
+
+  memset(server->addr_local, 0, sizeof(struct sockaddr_in));
+  memset(server->addr_local6, 0, sizeof(struct sockaddr_in));
+
+  server->socket = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
+  if (server->socket < 0)
+  {
+    server->socket = 0;
+    return messenger_error_create(NULL, 2);
+  }
+
+  server->socket6 = socket(AF_INET6, SOCK_DGRAM, IPPROTO_UDP);
+  if (server->socket < 0)
+  {
+    close(server->socket);
+    server->socket = 0;
+    server->socket6 = 0;
+    return messenger_error_create(NULL, 2);
+  }
+
+  return messenger_error_create(NULL, 0);
 }
 
-void messenger_create_client(messenger_client_t* client)
+messenger_error_t messenger_client_create(messenger_client_t* client)
 {
+  client->addr_local = malloc(sizeof(struct sockaddr_in));
+  client->addr_local6 = malloc(sizeof(struct sockaddr_in6));
+  client->addr_remote = malloc(sizeof(struct sockaddr_in));
+  client->addr_remote6 = malloc(sizeof(struct sockaddr_in6));
+
+  if (
+    !client->addr_local ||
+    !client->addr_local6 ||
+    !client->addr_remote ||
+    !client->addr_remote6
+  )
+  {
+    if (client->addr_local)
+      free(client->addr_local);
+    if (client->addr_local6)
+      free(client->addr_local6);
+    if (client->addr_remote)
+      free(client->addr_remote);
+    if (client->addr_remote6)
+      free(client->addr_remote6);
+    
+    return messenger_error_create(NULL, 1);
+  }
+
+  memset(client->addr_local, 0, sizeof(struct sockaddr_in));
+  memset(client->addr_local6, 0, sizeof(struct sockaddr_in));
+  memset(client->addr_remote, 0, sizeof(struct sockaddr_in));
+  memset(client->addr_remote6, 0, sizeof(struct sockaddr_in));
+
+  return messenger_error_create(NULL, 0);
 }
 
-void messenger_bind_server(messenger_server_t* server, uint16_t port)
+void messenger_server_destroy(messenger_server_t* server)
 {
+  if (server->addr_local)
+    free(server->addr_local);
+  if (server->addr_local6)
+    free(server->addr_local6);
 }
 
-void messenger_connect_client(messenger_client_t* client, const char* address, uint16_t port)
+void messenger_client_destroy(messenger_client_t* client)
+{
+  if (client->addr_local)
+    free(client->addr_local);
+  if (client->addr_local6)
+    free(client->addr_local6);
+  if (client->addr_remote)
+    free(client->addr_remote);
+  if (client->addr_remote6)
+    free(client->addr_remote6);
+}
+
+void messenger_server_bind(messenger_server_t* server, uint16_t port)
+{
+  server->addr_local->sin_family = AF_INET;
+  server->addr_local->sin_port = htons(server->port);
+  server->addr_local->sin_addr.s_addr = htonl(INADDR_ANY);
+
+  server->addr_local6->sin6_family = AF_INET6;
+  server->addr_local6->sin6_port = htons(server->port);
+  server->addr_local6->sin6_addr = in6addr_any;
+
+  if (bind(server->socket, (struct sockaddr*) server->addr_local, sizeof(struct sockaddr_in)) < 0)
+  {
+  }
+
+  if (bind(server->socket6, (struct sockaddr*) server->addr_local6, sizeof(struct sockaddr_in6)) < 0)
+  {
+  }
+}
+
+void messenger_client_connect(messenger_client_t* client, const char* address, uint16_t port)
 {
 }
